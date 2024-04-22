dacf89a5972c94512725db463c1802db5ba76fc6
ethpalser <eth.palser@gmail.com>
2024-04-22 01:15:05 -0400

    updated AVL tree balancing, issue with rebalance deeper up the tree

--- src/hash-tree/test_tree.py
+++ src/hash-tree/test_tree.py
@@ -1,8 +1,8 @@
-from unittest import TestCase
+import unittest
 
-from tree import Tree
+from tree import Tree, AVLTree
 
-class TestTree(TestCase):
+class TestTree(unittest.TestCase):
 
     def test_find_given_empty_tree_then_none(self):
         tree = Tree()
@@ -106,3 +106,130 @@ class TestTree(TestCase):
         self.assertTrue(result)
         actual = tree.find(9)
         self.assertEqual(tree.root.right, actual)
+
+class TestAVLTree(unittest.TestCase):
+
+    def test_left_child_root(self):
+        tree = AVLTree()
+        tree.insert(1, None)
+        self.assertRaises(Exception, lambda: tree._left_child(tree.root))
+
+    def test_rotate_left_root_of_empty_tree(self):
+        tree = AVLTree()
+        tree._rotate_left(tree.root)
+        self.assertEqual(tree.nil, tree.root)
+
+    def test_rotate_left_root_of_root_tree(self):
+        tree = AVLTree()
+        tree.insert(1, None)
+        tree._rotate_left(tree.root)
+        self.assertEqual(tree.find(1), tree.root)
+
+    def test_rotate_left_root_of_full_tree(self):
+        tree = AVLTree()
+        tree.insert(6, None) # root
+        tree.insert(4, None) # left
+        tree.insert(8, None) # right
+        tree.insert(2, None) # left.left
+        tree.insert(9, None) # right.right
+        tree.insert(5, None) # left.right
+        tree.insert(7, None) # right.left
+        # No rebalancing should have occurred
+        self.assertEqual(tree.find(6), tree.root)
+        tree._rotate_left(tree.root)
+        # The right is promoted up while the original is demoted to left
+        self.assertEqual(tree.find(8), tree.root)
+        self.assertEqual(tree.find(9), tree.root.right) # Unaffected by rotation
+        self.assertEqual(tree.find(6), tree.root.left)
+        self.assertEqual(tree.find(7), tree.root.left.right) # Affected by rotation
+
+    def test_rotate_right_root_of_empty_tree(self):
+        tree = AVLTree()
+        tree._rotate_right(tree.root)
+        self.assertEqual(tree.root, tree.nil)
+
+    def test_rotate_right_root_of_root_tree(self):
+        tree = AVLTree()
+        tree.insert(1, None)
+        tree._rotate_right(tree.root)
+        self.assertEqual(tree.find(1), tree.root)
+
+    def test_rotate_right_root_of_full_tree(self):
+        tree = AVLTree()
+        tree.insert(6, None) # root
+        tree.insert(4, None) # left
+        tree.insert(8, None) # right
+        tree.insert(2, None) # left.left
+        tree.insert(9, None) # right.right
+        tree.insert(5, None) # left.right
+        tree.insert(7, None) # right.left
+        # No rebalancing should have occurred
+        self.assertEqual(tree.find(6), tree.root)
+        tree._rotate_right(tree.root)
+        # The right is promoted up while the original is demoted to left
+        self.assertEqual(tree.find(4), tree.root)
+        self.assertEqual(tree.find(2), tree.root.left) # Unaffected by rotation
+        self.assertEqual(tree.find(6), tree.root.right)
+        self.assertEqual(tree.find(5), tree.root.right.left) # Affected by rotation
+    
+    def test_balance_tree_with_left_rotation(self):
+        tree = AVLTree()
+        tree.insert(1, None)
+        tree.insert(2, None)
+        tree.insert(3, None) # This will automatically rebalance once balance_tree is integrated
+        latest = tree.find(3)
+        tree._balance_tree(latest) # This does nothing if already balanced
+        self.assertEqual(tree.find(2), tree.root)
+        self.assertEqual(tree.find(1), tree.root.left)
+        self.assertEqual(tree.find(3), tree.root.right)
+
+    def test_balance_tree_with_right_rotation(self):
+        tree = AVLTree()
+        tree.insert(3, None)
+        tree.insert(2, None)
+        tree.insert(1, None) # This will automatically rebalance once balance_tree is integrated
+        latest = tree.find(1)
+        tree._balance_tree(latest) # This does nothing if already balanced
+        self.assertEqual(tree.find(2), tree.root)
+        self.assertEqual(tree.find(1), tree.root.left)
+        self.assertEqual(tree.find(3), tree.root.right)
+    
+    def test_balance_tree_with_left_right_rotation(self):
+        tree = AVLTree()
+        tree.insert(9, None)
+        tree.insert(5, None)
+        tree.insert(7, None) # This will automatically rebalance once balance_tree is integrated
+        latest = tree.find(7)
+        tree._balance_tree(latest) # This does nothing if already balanced
+        self.assertEqual(tree.find(7), tree.root)
+        self.assertEqual(tree.find(5), tree.root.left)
+        self.assertEqual(tree.find(9), tree.root.right)
+        
+    def test_balance_tree_with_right_left_rotation(self):
+        tree = AVLTree()
+        tree.insert(5, None)
+        tree.insert(9, None)
+        tree.insert(7, None) # This will automatically rebalance once balance_tree is integrated
+        latest = tree.find(7)
+        tree._balance_tree(latest) # This does nothing if already balanced
+        self.assertEqual(tree.find(7), tree.root)
+        self.assertEqual(tree.find(5), tree.root.left)
+        self.assertEqual(tree.find(9), tree.root.right)
+    
+    def test_balance_tree_with_many_imbalances(self):
+        tree = AVLTree()
+        tree.insert(1, None) # root
+        tree.insert(2, None) # balanced
+        tree.insert(3, None) # rotate left 2
+        tree.insert(4, None) # balanced
+        tree.insert(5, None) # rotate left 4
+        tree.insert(6, None) # rotate left 5
+        tree.insert(0, None)
+        tree.insert(7, None)
+        tree.insert(8, None)
+        tree.insert(9, None)
+        tree.insert(10, None)
+        self.assertEqual(tree.find(6), tree.root)
+
+if __name__ == '__main__':
+    unittest.main()
--- src/hash-tree/tree.py
+++ src/hash-tree/tree.py
@@ -1,4 +1,4 @@
-from node import Node, AVLNode
+from node import Node, AVLNode, RBNode
 
 class Tree:
 
@@ -16,7 +16,9 @@ class Tree:
         branch += "\n"
         for i in range(0, height):
             branch += "      "
-        branch += f"{node.key} <"
+        branch += f"k:{node.key}"
+        if node.left != self.nil or node.right != self.nil:
+            branch += " <"
         branch += self.__print_tree(height + 1, node.left)
         return branch
 
@@ -108,66 +110,185 @@ class Tree:
 
 class AVLTree(Tree):
 
-    def __rotate_left(self, node: Node):
-        if node is None or Node == self.nil:
+    def __init__(self):
+        self.nil = AVLNode(None, None)
+        self.root = self.nil
+
+    def find(self, key) -> AVLNode:
+        curr = self.root
+        while curr != self.nil and key != curr.key:
+            if key < curr.key:
+                curr = curr.left
+            else: # > curr.key
+                curr = curr.right
+        return curr
+        
+
+    def _left_child(self, node: Node) -> bool:
+        if node.parent is None:
+            raise Exception(f"This node does not have a parent. {node}")
+        match node:
+            case node.parent.left:
+                return True
+            case node.parent.right:
+                return False
+            case _:
+                raise Exception(f"Node is not a child of its parent.")
+
+    def _rotate_left(self, node: AVLNode):
+        if node is None or node == self.nil or node.right == self.nil:
             return
         temp = node.right.left
         node.right.left = node
         node.right.parent = node.parent
+        if node.parent is not None and node.parent != self.nil:
+            if self._left_child(node):
+                node.parent.left = node.right
+            else:
+                node.parent.right = node.right
+        else:
+            self.root = node.right
+
         node.parent = node.right
         node.right = temp
         node.update_height()
         node.parent.update_height()
 
-    def __rotate_right(self, node: Node):
-        if node is None or Node == self.nil:
+    def _rotate_right(self, node: AVLNode):
+        if node is None or node == self.nil or node.left == self.nil:
             return
         temp = node.left.right
         node.left.right = node
         node.left.parent = node.parent
+        if node.parent is not None:
+            if self._left_child(node):
+                node.parent.left = node.left
+            else:
+                node.parent.right = node.left
+        else:
+            self.root = node.left
+
         node.parent = node.left
         node.left = temp
-        node.parent.update_height()
         node.update_height()
+        node.parent.update_height()
 
-    def __balance_tree(self, node: AVLNode):
-        if node is None:
-            return
+    def _balance_tree(self, node: AVLNode):
+        grandparent = None
         parent = node.parent
         while parent is not None:
-            if __left_child(node):
+            grandparent = parent.parent
+            if self._left_child(node):
                 # parent is left heavy
                 if parent.balance() > 0:
+                    # child is right heavy, creating a gt-shape imbalance
                     if node.balance() < 0:
-                        self.__rotate_left(node)
-                        self.__rotate_right(parent) # Node's new parent after rotation
-                    else:
-                        self.__rotate_left(node)
+                        self._rotate_left(node)
+                        self._rotate_right(parent) # Node's new parent after rotation
+                    elif node.balance() > 0:
+                        self._rotate_right(parent)
                 else:
                     if parent.balance() < 0:
                         # height change is absorbed
                         break
-                    parent.update_height()
             else:
                 # parent is right heavy
                 if parent.balance() < 0:
+                    # child is left heavy, creating a lt-shape imbalance
                     if node.balance() > 0:
-                        self.__rotate_right(node)
-                        self.__rotate_left(parent)
-                    else:
-                        self.__rotate_right(node)
+                        self._rotate_right(node)
+                        self._rotate_left(parent)
+                    elif node.balance() < 0:
+                        self._rotate_left(parent)
                 else:
                     if parent.balance() > 0:
                         # height change is absorbed
                         break
-                    parent.update_height()
-            parent = parent.parent
-
-    def insert(self, key, data):
-        return super().insert(key, data)
+            node = parent
+            parent = grandparent
     
+    def insert(self, key, data):
+        if key is None:
+            raise Exception("key cannot be none")
+        new_node = AVLNode(key, data)
+        new_node.parent = None
+        new_node.left = self.nil
+        new_node.right = self.nil
+
+        parent = None
+        curr = self.root
+        while curr != self.nil:
+            parent = curr
+            if key < curr.key:
+                curr = curr.left
+            elif key > curr.key:
+                curr = curr.right
+            else:
+                # duplicate key, ignore insert
+                return
+        
+        if parent is None:
+            self.root = new_node
+        else:
+            new_node.parent = parent
+            if key < parent.key:
+                parent.left = new_node
+            else: # > parent.key
+                parent.right = new_node
+            to_resize = new_node
+            while to_resize != None:
+                to_resize.update_height()
+                to_resize = to_resize.parent
+        self._balance_tree(new_node)
+
     def delete(self, key) -> bool:
-        return super().delete(key)
+        to_delete: Node = self.find(key)
+        if to_delete == self.nil:
+            return False
+        
+        to_replace: Node
+        # This is a leaf node
+        if to_delete.left == self.nil and to_delete.right == self.nil:
+            to_replace = self.nil
+        # Only a left child
+        elif to_delete.left != self.nil and to_delete.right == self.nil:
+            to_replace = to_delete.left
+        # Only a right child
+        elif to_delete.right != self.nil and to_delete.left == self.nil:
+            to_replace = to_delete.right
+        # Has both children
+        else:
+            # Fetch the next largest node to replace the one being deleted
+            right_min: Node = to_delete.right
+            while right_min.left != self.nil:
+                right_min = right_min.left
+            # Move the replacing node's child to its location
+            if right_min.right != self.nil:
+                right_min.parent.left = right_min.right
+            right_min.left = to_delete.left
+            
+            # Determine if this is a child of the node being deleted
+            if to_delete.right != right_min:
+                right_min.right = to_delete.right
+            to_replace = right_min
+
+        if to_delete.parent is None:
+            self.root = to_replace
+        else:
+            # Determine which child of the parent this is
+            match to_delete:
+                case to_delete.parent.left:
+                    to_delete.parent.left = to_replace
+                case to_delete.parent.right:
+                    to_delete.parent.right = to_replace
+                case _:
+                    raise Exception("Something went wrong: Node has a parent, but is not one of its children.")
+        # Removing references for clean up
+        to_delete.left = None
+        to_delete.right = None
+        self._balance_tree(to_delete.parent)
+        del to_delete
+        return True
     
 class RBTree(Tree):
     
@@ -176,14 +297,3 @@ class RBTree(Tree):
     
     def delete(self, key) -> bool:
         return super().delete(key)
-
-def __left_child(node: Node) -> bool:
-    if node.parent is None:
-        raise Exception("This node does not have a parent.")
-    match node:
-        case node.parent.left:
-            return True
-        case node.parent.right:
-            return False
-        case _:
-            raise Exception("Node has a parent, but is not one of its children.")
