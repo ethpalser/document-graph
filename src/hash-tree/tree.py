afe1dd0cbbfb902b3c6f265fee9c3315c5077e7d
ethpalser <eth.palser@gmail.com>
2024-04-23 17:53:09 -0400

    reduced code duplication by moving node manipulating methods into Tree

--- src/hash-tree/tree.py
+++ src/hash-tree/tree.py
@@ -16,7 +16,7 @@ class Tree:
         branch += "\n"
         for i in range(0, height):
             branch += "      "
-        branch += f"k:{node.key},b:{node.balance()}"
+        branch += f"k:{node.key}"
         if node.left != self.nil or node.right != self.nil:
             branch += " <"
         branch += self.__print_tree(height + 1, node.left)
@@ -31,21 +31,33 @@ class Tree:
                 curr = curr.right
         return curr
     
-    def insert(self, key, data) -> Node:
+    def insert(self, key, data):
         if key is None:
             raise Exception("key cannot be none")
         new_node = Node(key, data)
         new_node.parent = None
         new_node.left = self.nil
         new_node.right = self.nil
+        return self._insert_node(new_node)
 
+    def delete(self, key) -> bool:
+        to_delete: Node = self.find(key)
+        replacement: Node = self._delete_node(to_delete)
+        # This is only when to_delete is None or nil (not found)
+        if replacement is None:
+            return False
+        return True
+
+    # General Node-manipulating methods
+
+    def _insert_node(self, new_node: Node):
         parent = None
         curr = self.root
         while curr != self.nil:
             parent = curr
-            if key < curr.key:
+            if new_node.key < curr.key:
                 curr = curr.left
-            elif key > curr.key:
+            elif new_node.key > curr.key:
                 curr = curr.right
             else:
                 # duplicate key, ignore insert
@@ -55,16 +67,14 @@ class Tree:
             self.root = new_node
         else:
             new_node.parent = parent
-            if key < parent.key:
+            if new_node.key < parent.key:
                 parent.left = new_node
             else: # > parent.key
                 parent.right = new_node
-        return new_node
-
-    def delete(self, key) -> bool:
-        to_delete: Node = self.find(key)
-        if to_delete == self.nil:
-            return False
+    
+    def _delete_node(self, to_delete: Node) -> Node:
+        if to_delete is None or to_delete == self.nil:
+            return None
         
         to_replace: Node
         # This is a leaf node
@@ -94,27 +104,22 @@ class Tree:
             self.root = to_replace
         else:
             # Determine which child of the parent this is
-            match to_delete:
-                case to_delete.parent.left:
-                    to_delete.parent.left = to_replace
-                case to_delete.parent.right:
-                    to_delete.parent.right = to_replace
-                case _:
-                    raise Exception("Something went wrong: Node has a parent, but is not one of its children.")
+            if self._left_child(to_delete):
+                to_delete.parent.left = to_replace
+            else:
+                to_delete.parent.right = to_replace
         # Update parent references
         to_replace.parent = to_delete.parent
         if to_delete.left is not None and to_delete.left != self.nil and to_delete.left != to_replace:
             to_delete.left.parent = to_replace
         if to_delete.right is not None and to_delete.right != self.nil and to_delete.right != to_replace:
             to_delete.right.parent = to_replace
-        self._balance_tree(to_replace)
         # Removing references for clean up
         to_delete.left = None
         to_delete.right = None
         del to_delete
-        return True
-
-    # General Node-manipulating methods
+        # Return the replacing node for processing
+        return to_replace
 
     def _left_child(self, node: Node) -> bool:
         if node.parent is None:
@@ -189,9 +194,12 @@ class AVLTree(Tree):
 
     def _balance_tree(self, node: AVLNode):
         if node is None:
-            return # Nothing can be done
+            return
+        # Ensure that this node has the correct height for balancing
+        node.update_height()
         while node.parent is not None:
-            parent = node.parent # maintain record, as it will change for node
+            parent = node.parent # Maintain record, as it will change for node
+            parent.update_height() # This height may have changed from its child's height changing
             if self._left_child(node):
                 # left imbalance
                 if parent.balance() > 1:
@@ -226,83 +234,15 @@ class AVLTree(Tree):
         new_node.left = self.nil
         new_node.right = self.nil
 
-        parent = None
-        curr = self.root
-        while curr != self.nil:
-            parent = curr
-            if key < curr.key:
-                curr = curr.left
-            elif key > curr.key:
-                curr = curr.right
-            else:
-                # duplicate key, ignore insert
-                return
-        
-        if parent is None:
-            self.root = new_node
-        else:
-            new_node.parent = parent
-            if key < parent.key:
-                parent.left = new_node
-            else: # > parent.key
-                parent.right = new_node
-            to_resize = new_node
-            while to_resize != None:
-                to_resize.update_height()
-                to_resize = to_resize.parent
+        super()._insert_node(new_node)
         self._balance_tree(new_node)
 
     def delete(self, key) -> bool:
         to_delete: Node = self.find(key)
-        if to_delete == self.nil:
+        replacement: Node = super()._delete_node(to_delete)
+        if replacement is None:
             return False
-        
-        to_replace: Node
-        # This is a leaf node
-        if to_delete.left == self.nil and to_delete.right == self.nil:
-            to_replace = self.nil
-        # Only a left child
-        elif to_delete.left != self.nil and to_delete.right == self.nil:
-            to_replace = to_delete.left
-        # Only a right child
-        elif to_delete.right != self.nil and to_delete.left == self.nil:
-            to_replace = to_delete.right
-        # Has both children
-        else:
-            # Fetch the next largest node to replace the one being deleted
-            right_min: Node = to_delete.right
-            while right_min.left != self.nil:
-                right_min = right_min.left
-
-            if to_delete.right != right_min:
-                # Move the replacing node's only potential child to take its place
-                right_min.parent.left = right_min.right
-                right_min.right = to_delete.right
-            right_min.left = to_delete.left
-            to_replace = right_min
-
-        if to_delete.parent is None:
-            self.root = to_replace
-        else:
-            # Determine which child of the parent this is
-            match to_delete:
-                case to_delete.parent.left:
-                    to_delete.parent.left = to_replace
-                case to_delete.parent.right:
-                    to_delete.parent.right = to_replace
-                case _:
-                    raise Exception("Something went wrong: Node has a parent, but is not one of its children.")
-        # Update parent references
-        to_replace.parent = to_delete.parent
-        if to_delete.left is not None and to_delete.left != self.nil and to_delete.left != to_replace:
-            to_delete.left.parent = to_replace
-        if to_delete.right is not None and to_delete.right != self.nil and to_delete.right != to_replace:
-            to_delete.right.parent = to_replace
-        self._balance_tree(to_replace)
-        # Removing references for clean up
-        to_delete.left = None
-        to_delete.right = None
-        del to_delete
+        self._balance_tree(replacement)
         return True
     
 class RBTree(Tree):
