93913f6f529f1a53c13c4c2b28651b4ecfcc0902
ethpalser <eth.palser@gmail.com>
2024-05-02 12:13:11 -0400

    updated tree map tests for insert

--- src/tree-map/test_tree_map.py
+++ src/tree-map/test_tree_map.py
@@ -31,5 +31,31 @@ class TestTreeMap(unittest.TestCase):
         actual = map.find(3)
         self.assertIsNone(actual)
 
+    def test_insert_given_empty(self):
+        map = TreeMap()
+        map.insert(1, None)
+        # This access is not recommended, but checking that it exists in tree
+        self.assertIsNotNone(map.tree.root)
+        # This access is not recommended, but checking that it exists in dictionary
+        self.assertIsNotNone(map.map[1])
+
+    def test_insert_given_tree_of_one(self):
+        map = TreeMap()
+        map.insert(1, None)
+        map.insert(2, None)
+        # This access is not recommended, but checking that it exists in tree
+        self.assertIsNotNone(map.tree.root.right)
+        # This access is not recommended, but checking that it exists in dictionary
+        self.assertIsNotNone(map.map[2])
+
+    def test_insert_given_tree_of_many(self):
+        map = TreeMap()
+        map.insert(1, None)
+        map.insert(2, None)
+        map.insert(3, None)
+        # Not checking the tree, as its structure can change from rebalancing
+        # This access is not recommended, but checking that it exists in dictionary
+        self.assertIsNotNone(map.map[3])
+
 if __name__ == '__main__':
     unittest.main()
